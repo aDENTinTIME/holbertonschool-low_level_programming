8c81f0220cc4c02870c705e234afc1180853813c
adentintime <arixbizz@gmail.com>
2018-01-19 04:37:27 +0000

    5-6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet.
+* Return: 0
+*/
+
+int main(void)
+{
+	char d;
+
+	d = 48;
+
+	while (d <= 57)
+	{
+	putchar(d);
+	d++;
+	}
+
+	putchar('\n');
+	return (0);
+}
