648a6c7f62791441af4eb0c0de17b45d9fa7d472
adentintime <arixbizz@gmail.com>
2018-01-24 04:49:00 +0000

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* print_last_digit - Prints the last digit of the number fed in.
+* @n: is being passed in.
+* Return: -n or n.
+*/
+
+int print_last_digit(int n)
+{
+	return (n%10);
+}
