8f1a0db68c3fffbd7ee1889f3e9952c5a248f8d6
adentintime <arixbizz@gmail.com>
2018-01-30 22:43:34 +0000

    0

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - Changes the value of a pointer.
+* @a: first charater fed in.
+* @b: second charater fed in.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
