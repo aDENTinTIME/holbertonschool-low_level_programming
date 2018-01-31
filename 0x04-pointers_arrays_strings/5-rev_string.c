0613220a6f7f01d9e23e1c16ee4c44e0913bf5a6
adentintime <arixbizz@gmail.com>
2018-01-31 07:03:59 +0000

    5

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* rev_string - Reverses a string, letter by letter.
+* @s: string passed to function.
+*/
+
+void rev_string(char *s)
+{
+	int i, d, med;
+
+	i = 0;
+	d = 0;
+	med = 0;
+
+	for (; s[d] != '\0'; ++d)
+	{
+	}
+	d -= 1;
+	while (i != d)
+	{
+		med = s[i];
+		s[i] = s[d];
+		s[d] = med;
+
+		d--;
+		i++;
+	}
+}
