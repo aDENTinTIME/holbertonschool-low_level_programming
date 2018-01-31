cd01d4bd22f436b499d096985110ce36c5a1d3b1
adentintime <arixbizz@gmail.com>
2018-01-31 07:19:29 +0000

    6.2 ugly patch

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,14 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] > '\0'; i += 2)
+	for (i = 0; str[i] != '\0';)
 	{
 		_putchar(str[i]);
+		i++;
+		if (str[i] == '\0')
+		break;
+
+		i++;
 	}
 	_putchar('\n');
 }
--- 0x04-pointers_arrays_strings/main.c
+++ 0x04-pointers_arrays_strings/main.c
@@ -9,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    str = "0123456789";
+    str = "01";
     puts2(str);
     return (0);
 }
