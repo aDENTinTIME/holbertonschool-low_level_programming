330bc3b7e5db34e8c8da2ea135fe23f62b4529df
adentintime <arixbizz@gmail.com>
2018-02-01 05:18:17 +0000

    9.1 betty

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 
 /**
-* _strcpy -
-* @dest:
-* @src:
+* _strcpy - copies contents of one string to another.
+* @dest: destination string.
+* @src: source string.
+* Return: copied string.
 */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for(i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	return(dest);
+	return (dest);
 }
