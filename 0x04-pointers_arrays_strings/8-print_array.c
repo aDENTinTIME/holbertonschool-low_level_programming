fe3c8294df36df0ce67591e07d3d54411e98128e
adentintime <arixbizz@gmail.com>
2018-02-01 04:21:11 +0000

    8

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,21 @@
 #include <stdio.h>
 
 /**
-* print_array
+* print_array - prints all the values in an array.
+* @a: array fed in.
+* @n: length of array.
 */
 
 void print_array(int *a, int n)
 {
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
 }
