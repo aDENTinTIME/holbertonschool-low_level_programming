6d0a30b5314fe6423a071aaa6e14bf36dc78ad83
adentintime <arixbizz@gmail.com>
2018-02-02 04:22:15 +0000

    3.3 rewrote whole function to do what strcmp is supposed to do.

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,37 +1,28 @@
 #include "holberton.h"
 
 /**
-* _strcmp - Appends string src to dest, up to a given number of characters.
-* @s1: String to be appended.
-* @s2: Source string of appendation.
-* Return: Appended string.
+* _strcmp - Compares string values.
+* @s1: String 1.
+* @s2: String 2.
+* Return: 15 if s1>s2; -15 if s1<s2; 0 if equal.
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	int val_1, val_2, index;
+	int index;
 
-	val_1 = 0;
-	val_2 = 0;
-
-	for (index = 0; s1[index] != '\0'; index++)
+	for (index = 0; s1[index] != '\0' || s2[index] != '\0'; index++)
 	{
-		val_1 += s1[index];
-	}
-	for (index = 0; s2[index] != '\0'; index++)
-	{
-		val_2 += s2[index];
-	}
-
-	if (val_1 > val_2)
-	{
-		return (15);
-	} else if (val_1 < val_2)
-	{
-		return (-15);
-	} else if (val_1 == val_2)
-	{
-		return (0);
+		if (s1[index] > s2[index])
+		{
+			return (15);
+		} else if (s1[index] < s2[index])
+		{
+			return (-15);
+		} else if (s1[index] == s2[index])
+		{
+			return (0);
+		}
 	}
 
 	return (0);
