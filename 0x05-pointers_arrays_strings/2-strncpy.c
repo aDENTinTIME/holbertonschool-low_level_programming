c6f08e1299b3a23c95b9a20b315b08fd46b154d4
adentintime <arixbizz@gmail.com>
2018-02-02 03:16:38 +0000

    9.3 I find this function to act strange, but I guess it makes sense

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,18 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int count;
 
-	for (count = 0; src[count] != '\0' && count < n; count++)
+	for (count = 0; count < n; count++)
 	{
-		dest[count] = src[count];
-	}
-	if (src[count] == '\0')
-	{
-		dest[count] = '\0';
+		if (src[count] != '\0')
+		{
+			dest[count] = src[count];
+		} else
+		{
+			for (; count < n; count++)
+			{
+				dest[count] = '\0';
+			}
+		}
 	}
 	return (dest);
 }
