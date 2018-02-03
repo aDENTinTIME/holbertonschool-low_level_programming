4dde1e886bf3dcb5e2f73d03ab514d11f2a42c7a
adentintime <arixbizz@gmail.com>
2018-02-03 07:54:43 +0000

    100

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* print_number - Prints the number given.
+* @n: number.
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar ('-');
+	}
+
+	if (n > 9999)
+		_putchar (n % 100000 / 10000 + '0');
+
+	if (n > 999)
+		_putchar (n % 10000 / 1000 + '0');
+
+	if (n > 99)
+		_putchar (n % 1000 / 100 + '0');
+
+	if (n > 9)
+		_putchar (n % 100 / 10 + '0');
+
+	_putchar (n % 10 + '0');
+}
