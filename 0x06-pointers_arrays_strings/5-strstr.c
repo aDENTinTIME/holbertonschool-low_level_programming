39f28e0591033f3d8461c8b7b3cc711b13091cdd
adentintime <arixbizz@gmail.com>
2018-02-06 04:44:17 +0000

    5.1

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
 * _strstr - function finds the first occurrence
 * of the substring needle in the string haystack.
 * @haystack: Source string.
-* @needle: Characters to look for.
+* @needle: String to look for.
 * Return: Remaining string from point character occurs.
 */
 
@@ -14,9 +14,9 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (ii = 0; needle[ii] == haystack[i + ii] ; ii++)
+		for (ii = 0; needle[ii] == haystack[i + ii]; ii++)
 		{
-			if (needle[ii] == '\0')
+			if (needle[ii + 1] == '\0')
 				return (haystack + i);
 		}
 	}
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -10,7 +10,7 @@
 int main(void)
 {
     char *s = "hello, worldhi eh";
-    char *f = "world";
+    char *f = "worl";
     char *t;
 
     t = strstr(s, f);
