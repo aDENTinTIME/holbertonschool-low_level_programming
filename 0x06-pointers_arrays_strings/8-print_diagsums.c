fbfd79b20a16d9df8c23cbb2f9d10057013e1a9f
adentintime <arixbizz@gmail.com>
2018-02-06 07:10:33 +0000

    9

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strstr - function finds the first occurrence
+* of the substring needle in the string haystack.
+* @haystack: Source string.
+* @needle: String to look for.
+* Return: Remaining string from point character occurs.
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, ii, value1;
+
+	i = 0;
+	ii = 0;
+	value1 = 0;
+
+	printf("%d\n", *a[0][0]);
+
+	while (i < size)
+	{
+		i++;
+		ii++;
+	}
+	printf("%d\n", value1);
+}
---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* set_string - Sets the value of a pointer to a char.
+* @s: Source string.
+* @to: String to replace.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
