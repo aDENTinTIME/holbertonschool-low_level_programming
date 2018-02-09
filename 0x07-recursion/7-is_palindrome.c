43e58755b256ac2f433091b494f2038bb4abec54
adentintime <arixbizz@gmail.com>
2018-02-09 03:59:18 +0000

    7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
-* stuff
-* Not allowed to use loops.
-* @x: Number.
-* @y: Power.
-* Return: Fully powered number.
+* len_check - Helper function to check the length of a string.
+* @s: String.
+* Return: Length of string.
 */
 
 int len_check(char *s)
@@ -18,28 +16,47 @@ int len_check(char *s)
 	return (0);
 }
 
+/**
+* pal_check - Helper function doing the actual work,
+* determins if sting is palindrome.
+* @length: Last position in string.
+* @start: First position in string.
+* @s: String.
+* Return: A boolean value.
+*/
+
 int pal_check(int length, int start, char *s)
 {
 	if (s[length] != s[start])
-	{       
+	{
 		return (0);
 	}
 
-	if (start >= length)
+	if (start > length / 2)
 	{
 		return (1);
 	}
+	else
+	{
+		return (pal_check(length - 1, start + 1, s));
+	}
 
-	return (pal_check(length--, start++, s));
+	return (0);
 }
+
+/**
+* is_palindrome - Primary function to be called,
+* determins if sting is palindrome.
+* Not allowed to use loops.
+* @s: String.
+* Return: 1 if palindrome; 0 if not.
+*/
 
 int is_palindrome(char *s)
 {
 	int length;
 
 	length = len_check(s) - 1;
-
-	printf("%d", length);
 
 	return (pal_check(length, 0, s));
 }
