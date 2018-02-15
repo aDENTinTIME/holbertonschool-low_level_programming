4f31114d090ff1698f450fccbda0670aff9f2043
adentintime <arixbizz@gmail.com>
2018-02-15 07:58:15 +0000

    3.5

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		*(grid + i) = malloc(width * sizeof(int));
 	}
 
-	if (grid == NULL)
+	if (*grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height * width; i++)
