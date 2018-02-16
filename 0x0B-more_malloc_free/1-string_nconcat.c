f8c973cbf14ee5d8eb5092a210cd94a58ac4aafc
adentintime <arixbizz@gmail.com>
2018-02-16 04:06:31 +0000

    1

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* len - Helper function to measure string length.
+* @s: Passed in string.
+* Return: Length as number.
+*/
+
+int len(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+* string_nconcat - Concatenates two strings.
+* @s1: Main string.
+* @s2: String to draw from.
+* @n: Number of chars to take from s2.
+* Return: Pointer to concatenated string.
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i, ii;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	ptr = malloc(len(s1) * len(s2) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (ii = 0; ii < n && s2[ii] != '\0'; ii++)
+	{
+		ptr[i] = s2[ii];
+		i++;
+	}
+
+	return (ptr);
+}
