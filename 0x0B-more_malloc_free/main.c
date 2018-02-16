6bd503d8fb99fd4e2eadb97ff48119deb6073716
adentintime <arixbizz@gmail.com>
2018-02-16 04:08:50 +0000

    1.1 added null terminator

--- 0x0B-more_malloc_free/main.c
+++ 0x0B-more_malloc_free/main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat("Holberton ", "hi", NULL);
+    concat = string_nconcat("Holberton ", "hi", 5);
     printf("%s\n", concat);
     free(concat);
     return (0);
