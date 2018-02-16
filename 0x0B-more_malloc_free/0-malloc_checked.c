490243feeeca9aeca76149ea95ffc8d14879b3b1
adentintime <arixbizz@gmail.com>
2018-02-16 02:22:08 +0000

    0

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* malloc_checked - Allocates memory using malloc.
+* @b: Desired number of bytes.
+* Return: Void pointer to newly allocated memory address.
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr != NULL)
+	{
+		return (ptr);
+	}
+	else
+	{
+		exit (98);
+	}
+}
