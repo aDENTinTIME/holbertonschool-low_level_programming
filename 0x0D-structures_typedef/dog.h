11ba699dc0f8a3d59e49f477f355bba2db02fd67
adentintime <arixbizz@gmail.com>
2018-02-20 05:03:03 +0000

    inital commit, first problem

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+int _putchar(char c);
+
+
+/**
+* struct dog - Holds info about dogs.
+* @name: Name of dog.
+* @age: Age of dog.
+* @owner: Name of owner.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
