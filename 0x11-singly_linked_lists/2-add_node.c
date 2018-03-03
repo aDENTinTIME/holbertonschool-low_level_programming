f00c7875a3213e44cfd03507d11bede2bf2d0dde
adentintime <arixbizz@gmail.com>
2018-03-03 03:45:31 +0000

    2.2 and 3.2 actually returning address of the new element

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -57,5 +57,5 @@ list_t *add_node(list_t **head, const char *str)
 
 	*head = new;
 
-	return (&new);
+	return (new);
 }
--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -66,5 +66,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		}
 		temp->next = new;
 	}
-	return (&new);
+	return (new);
 }
