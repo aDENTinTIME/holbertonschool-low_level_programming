071587dcdd93198b5b1933884fd2388f00de0038
adentintime <arixbizz@gmail.com>
2018-03-03 06:25:45 +0000

    101 working

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+;Some notes, because assembly is crazy.
+
+        extern	printf				; Call printf
+
+        section .data				; Data section, initialized variables
+msg:	db "Hello, Holberton", 0	; C string needs 0
+fmt:    db "%s", 10, 0				; The printf format, "\n",'0'
+
+        section .text				; Code section.
+
+        global main					; the standard gcc entry point
+main:								; the program label for the entry point
+        push    rbp					; set up stack frame, must be alligned
+	
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0						; or can be  xor  rax,rax
+        call    printf				; Call printf, again
+
+	pop	rbp							; restore stack
+
+	mov	rax,0						; normal, no error, return value
+	ret								; return, yay we're done!
