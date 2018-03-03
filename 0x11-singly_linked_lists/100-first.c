00a1dc7a36d44e7cccc7d2de5843e28e77d0cebf
adentintime <arixbizz@gmail.com>
2018-03-03 06:08:34 +0000

    100 working and betty

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* me_first - Runs first.
+*/
+
+__attribute__ ((constructor))
+void me_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
