68cb2f9557649d80fa459359598094ef00b3a7ac
adentintime <arixbizz@gmail.com>
2018-03-07 07:22:43 +0000

    9.1 Working on better protection against edge cases

--- 0x12-more_singly_linked_lists/test.c
+++ 0x12-more_singly_linked_lists/test.c
@@ -1,18 +1,10 @@
 #include "lists.h"
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-	{
-	}
-
-	return (i);
-}
 
 int main(void)
 {
-	_putchar(_strlen("hell") + '0');
+	int *ptr = NULL;
+
+	printf("%p\n", &ptr);
 
 	return (0);
 }
