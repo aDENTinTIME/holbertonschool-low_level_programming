0c43e67f78f05851d81f92f21027de00e39f3abb
adentintime <arixbizz@gmail.com>
2018-03-08 07:42:43 +0000

    100 partly working

---
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+* reverse_listint - 
+* @head: Address of pointer to struct of type listint_t.
+*
+* Return: Address of new first node.
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *uno, *dos;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (NULL);
+
+	uno = *head;
+	dos = *head;
+
+	while (uno->next != NULL)
+	{
+		if (uno->next->next == NULL)
+		{
+			uno->next->next = uno;
+			uno = uno->next;
+			uno->next->next = NULL;
+			dos = uno;
+			uno = *head;
+		}
+		else
+			uno = uno->next;
+	}
+
+	*head = dos;
+
+	return (*head);
+}
