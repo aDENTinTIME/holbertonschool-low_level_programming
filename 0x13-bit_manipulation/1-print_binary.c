45873903e5fcedb647bd1544b5f45b19c9fd82aa
adentintime <arixbizz@gmail.com>
2018-03-10 01:38:46 +0000

    1 change so that we can print unsigned long int, and removed extraneous code

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,29 +1,17 @@
 #include "holberton.h"
 
 /**
-* binary_helper - Converts and prints base 10 in base 2.
-* @num: Argument of type int.
-*
-* Return: Number of characters printed.
-*/
-
-unsigned int binary_helper(unsigned int num)
-{
-	if (num < 2)
-	{
-		_putchar(num + '0');
-		return (1);
-	}
-	return (1 + binary_helper(num >> 1) + !_putchar((num & 1) + '0'));
-}
-
-
-/**
 * print_binary - Prints the binary representation of a number.
 * @n: Int to be printed.
 */
 
 void print_binary(unsigned long int n)
 {
-	binary_helper(n);
+	if (n < 2)
+	{
+		_putchar(n + '0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
