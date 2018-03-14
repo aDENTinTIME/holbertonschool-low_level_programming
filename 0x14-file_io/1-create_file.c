5f685c2000bad50ea2a5eb405231793071b4481e
adentintime <arixbizz@gmail.com>
2018-03-14 06:35:36 +0000

    1 working without truncation

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -25,11 +25,11 @@ int create_file(const char *filename, char *text_content)
 	{
 	}
 
-	fd = open(filename, O_RDWR | O_TRUNC, 0 | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_CREAT, 00600);
 	if (fd == -1)
 		return (-1);
 
-	if ((write(fd, text_content, length)) == -1)
+	if ((write(fd, text_content, (length - 1))) == -1)
 		return (-1);
 
 	close(fd);
