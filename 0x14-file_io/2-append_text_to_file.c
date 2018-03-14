7a720414b30a9a167fbc9e04b4c04395b66a99cb
adentintime <arixbizz@gmail.com>
2018-03-14 18:57:24 +0000

    2 was working, changed logic

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -11,23 +11,27 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, length;
+	int fd;
+	unsigned int length;
 
 	length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
 	if (text_content == NULL)
-		text_content = "";
+	{
+		close(fd);
+		return (1);
+	}
 
 	while (text_content[length++])
 	{
 	}
-
-	fd = open(filename, O_RDWR | O_APPEND);
-	if (fd == -1)
-		return (-1);
 
 	if ((write(fd, text_content, (length - 1))) == -1)
 		return (-1);
