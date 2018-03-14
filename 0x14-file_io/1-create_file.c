845e4c6c64c3f01378c3ed257251747a01277cfb
adentintime <arixbizz@gmail.com>
2018-03-14 18:53:37 +0000

    1 now compiles

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,7 +11,8 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	unsigned int fd, length;
+	int fd;
+	unsigned int length;
 
 	length = 0;
 
@@ -24,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		close(fd)
+		close(fd);
 		return (1);
 	}
 
