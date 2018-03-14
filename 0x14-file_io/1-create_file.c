751b8a1d5fe5f04962ea045a181fb89bfaca98bb
adentintime <arixbizz@gmail.com>
2018-03-14 18:49:38 +0000

    1 change logic, add TRUNC

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,23 +11,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, length;
+	unsigned int fd, length;
 
 	length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 00600);
+	if (fd == -1)
+		return (-1);
+
 	if (text_content == NULL)
-		text_content = "";
+	{
+		close(fd)
+		return (1);
+	}
 
 	while (text_content[length++])
 	{
 	}
-
-	fd = open(filename, O_RDWR | O_CREAT, 00600);
-	if (fd == -1)
-		return (-1);
 
 	if ((write(fd, text_content, (length - 1))) == -1)
 		return (-1);
