0608bf87e05045db38a14de38588062a70c7d58d
adentintime <arixbizz@gmail.com>
2018-04-16 01:20:21 +0000

    7-insert_dnodeint.c v2 step back

--- 0x16-doubly_linked_lists/wip_7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/wip_7-insert_dnodeint.c
@@ -1,25 +1,21 @@
 #include "lists.h"
 
 /**
- * name
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
  *
- * @head: Head of list.
+ * @h: Head of list.
+ * @idx: Position in linked list to travle to.
  * @n: Number to be stored in node.
- * Return: Pointer to new node.
+ * Return: New node at index or NULL if index out of range.
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *slow, *fast;
-	unsigned int i;
-
-	slow = *h;
-	fast = *h;
-	i = idx;
+	dlistint_t *fast = *h, *slow = *h, *new;
+	unsigned int i = idx - 1;
 
 	if (!h)
 		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
@@ -34,28 +30,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			fast->prev = new;
 		return (new);
 	}
-	if (fast == NULL)
+
+	if (!fast)
 		return (NULL);
 
 	fast = fast->next;
 	while (i--)
 	{
-		if (fast == NULL)
+		if (!fast)
 			return (NULL);
 		fast = fast->next;
 		slow = slow->next;
 	}
 
-	fast = fast->prev;
-	slow = slow->prev;
-
 	new->n = n;
 	new->next = fast;
 	new->prev = slow;
-
 	if (new->next != NULL)
 		new->next->prev = new;
-
 	slow->next = new;
 
 	return (new);
