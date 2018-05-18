77b57dfdf4e5edc21683711a2a3ef520872a53f6
adentintime <arixbizz@gmail.com>
2018-05-18 03:51:41 +0000

    Update 0, initialize array with NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
+	unsigned long int i;
 
 	new_ht = malloc(sizeof(hash_table_t));
 	if (!new_ht)
@@ -18,6 +19,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_ht->array)
 		return (NULL);
+
+	i = 0;
+	while (i < size)
+	{
+		new_ht->array[i] = NULL;
+		i++;
+	}
 
 	return (new_ht);
 }
