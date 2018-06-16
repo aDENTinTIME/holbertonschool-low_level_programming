9cbffb04488efc84865d6bc3c391345b45e50a13
adentintime <arixbizz@gmail.com>
2018-06-16 06:57:34 +0000

    Create all project files, some empty

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""5-island_perimeter"""
+
+
+def island_perimeter(grid):
+    """island_perimeter
+    Returns the perimeter of the island described in grid
+    """
+    print(len(grid) * len(grid))
